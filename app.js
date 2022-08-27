208d1d335e3c85a8e0d2d662f1e371a712f3a770
Koray-K <kurucankoray@gmail.com>
2022-08-27 19:53:01 +0300

    bazı ui güncellemeleri ve oyun mantığı değiştirildi

--- app.js
+++ app.js
@@ -8,63 +8,67 @@ function getRandomInt(min, max) {
 function computerPlay() {
     let randomNumber = getRandomInt(1,3);
     if (randomNumber === 1) {
-        return 'rock'
+        return 'Rock'
     } else if (randomNumber === 2){
-        return 'paper'
+        return 'Paper'
     } else {
-        return 'scissors'
+        return 'Scissors'
     }
 }
 
-// One hand game play
 
 let playerCount = 0
 let computerCount = 0
 
-function playRound(playerSelection, computerSelection) {
-    let loseMessage = `You Lose! ${computerSelection} beats ${playerSelection}`
-    let winMessage = `You Win! ${playerSelection} beats ${computerSelection}`
-    let tieMessage = `Tie ${playerSelection} and ${computerSelection} are same ! `
 
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        playerCount +=1
-        return winMessage;
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        computerCount +=1
-        return loseMessage;
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        playerCount +=1
-        return winMessage;
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
-        computerCount +=1
-        return loseMessage;
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        playerCount +=1
-        return winMessage;
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-        computerCount +=1
-        return loseMessage;
-    } else {
-        return tieMessage;
+const rock = document.querySelector("#rock");
+const rockValue = document.querySelector("#rock").value;
+const paper = document.querySelector("#paper");
+const paperValue = document.querySelector("#paper").value;
+const scissors = document.querySelector("#scissors")
+const scissorsValue = document.querySelector("#scissors").value;
+
+
+
+function playRound(playerSelection, computerSelection, playerCount, computerCount) {
+    let displayRound = document.querySelector("div.round-display");
+    let playerScore = document.getElementById("display-player");
+    let computerScore = document.getElementById("display-computer");
+
+    if(playerSelection == computerSelection) {
+        displayRound.innerHTML = `The player played ${playerSelection} and the computer played ${computerSelection}! It's a Tie`;
+    }else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        displayRound.innerHTML = `The player played ${playerSelection} and the computer played ${computerSelection}! The player won!`
+        playerCount += 1;
+    }else {
+        displayRound.innerHTML = `The player played ${playerSelection} and the computer played ${computerSelection}! The computer won!`;
+       computerCount += 1;
     }
-}
-// Win , Lose And Tie Messages. 
+    computerScore.innerHTML = `${computerCount}`
+    playerScore.innerHTML = `${playerCount}`
+   
 
-computerCount = 0
-playerCount = 0
+    return playerCount, computerCount;
+}
 
 function game() {
-    for (let i = 0; i< 5; i++) {
-        let playerSelection = prompt('Rock Paper Scissors')
-        let computerSelection = computerPlay()
-        console.log(playRound(playerSelection, computerSelection))
-        console.log(`Your Score ${playerCount}`)
-        console.log(`Computer Score ${computerCount}`)
-    }
-    computerCount = 0
-    playerCount = 0
-    confirm('Do you want to play again? ')
-    console.log(game())
+
+    rock.addEventListener("click", () => {
+        let computerInput = computerPlay();
+        playRound(rockValue, computerInput, playerCount, computerCount)
+    })
+    
+    paper.addEventListener("click", () => {
+        let computerInput = computerPlay();
+        playRound(paperValue, computerInput, playerCount, computerCount)
+        console.log("1")
+    })
+    
+    scissors.addEventListener("click", () => {
+        let computerInput = computerPlay();
+        playRound(scissorsValue, computerInput, playerCount, computerCount)   
+    })
+
 }
 
-console.log(game())
+game()
